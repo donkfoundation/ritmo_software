190b7ad80a15f540d3fbcedac36b6d2aa3529727
Camilo Rojas <camilorojas@inemjose.edu.co>
2023-12-13 14:35:50 -0500

    Ultimas correcciones

--- SQL/script.sql
+++ SQL/script.sql
@@ -359,20 +359,6 @@ BEGIN
 END;
 GO
 
-CREATE PROCEDURE ObtenerInventarioDisponibles
-AS
-BEGIN
-	SELECT 
-		descripcion,
-		cantidad,
-		estado,
-		nit_proveedor,
-		fecha_ingreso,
-		fecha_egreso
-	FROM Inventario WHERE estado = 'D'
-END;
-GO
-
 CREATE PROCEDURE ObtenerTotalCuentasUltimoMes
 AS
 BEGIN
@@ -424,3 +410,44 @@ BEGIN
 	RETURN @diferencia
 END;
 GO
+
+
+INSERT INTO Pais (cod_pais, nom_pais)
+VALUES
+    (57, 'COLOMBIA'),
+    (1, 'USA'),
+    (34, 'SPAIN'),
+    (86, 'CHINA'),
+	(38, 'YUGOSLAVIA');
+
+INSERT INTO Dpto (nom_dpto, cod_pais)
+VALUES ('ANTIOQUIA', 57);
+
+INSERT INTO Ciudad (nom_ciudad, cod_dpto)
+VALUES ('MEDELLIN', 1);
+
+INSERT INTO DescripHabitacion ( tipo_hab, descripcion)
+VALUES
+    ( 'INDIVIDUAL', 'UNA CAMA SIMPLE, TELEVISOR, MESA'),
+    ( 'PAREJA', 'UNA CAMA DOBLE, TELEVISOR, COMEDOR, MESA'),
+    ( 'FAMILIAR', 'DOS CAMAS SIMPLES Y UNA DOBLE, COMEDOR, DOS BAÑOS');
+
+INSERT INTO Habitaciones (num_hab, tipo_hab, estado, capacidad, precio)
+VALUES
+	(101, 'INDIVIDUAL', 'D', 1, 10000),
+	(102, 'PAREJA', 'D', 2, 20000),
+	(103, 'FAMILIAR', 'D', 5, 30000),
+	(104, 'INDIVIDUAL', 'D', 5, 30000);
+
+INSERT INTO Roles (cod_rol, tipo_rol)
+VALUES
+	(1, 'ADMIN'),
+	(2, 'MANAGER'),
+	(3, 'STAFF'),
+	(4, 'RECEPCIONISTA');
+
+INSERT INTO Paquetes (nombre_paquete, activo, descripcion, precio_costo, iva, precio_total)
+VALUES
+    ('BASICO', 'S', 'DESAYUNO, ALMUERZO, CENA', 50000, 10000, 60000),	
+    ('ESTANDAR', 'S', 'DESAYUNO, ALMUERZO, CENA, ACCESO A COMODIDADES', 100000, 20000, 120000),
+    ('PREMIUM', 'S', 'DESAYUNO, ALMUERZO, CENA, ACCESO A COMODIDADES, MASAJES INCLUIDOS', 120000, 24000, 160000);
